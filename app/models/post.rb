b89b531b14e710cc2f3b895a397bbdf2921a1916
Kai L <kailin8591@gmail.com>
2022-08-15 11:06:01 -0400

    commit tree 60292ff96dca6b108d2c56cfc1ec44ba0d2c4a00

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: [ 'Fiction', 'Non-Fiction' ] }
+
+  validate :clickbaits
+
+  ClickPattern = [
+    /Wont's Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+
+  ]
+
+  def clickbaits
+    if ClickPattern.none? { |pattern| pattern.match title }
+    errors.add(:title, "must be clickbaits")
+    end
+  
+  end
+
 end
